82a8ae95a1d77f7c66b5895140f790a05507ce8e
Wil Gaboury <wgaboury@gmail.com>
2026-06-06 23:15:10 -0400

    Add meta modification

--- src/meta.rs
+++ src/meta.rs
@@ -1,12 +1,12 @@
-use std::{fs::File, os::unix::fs::FileExt, path::Path};
+use std::{fs::File, os::unix::fs::FileExt};
 
 use anyhow::{Result, bail};
 use tokio::sync::Mutex;
 
 use crate::{
     const_assert,
-    fio::{MAX_PAGE_SIZE, MIN_PAGE_SIZE},
-    util::{CHECKSUM_SIZE, from_bytes, has_valid_checksum},
+    fio::{Fio, MAX_PAGE_SIZE, MIN_PAGE_SIZE},
+    util::{CHECKSUM_SIZE, from_bytes, from_bytes_mut, has_valid_checksum, update_checksum},
 };
 
 type MagicType = u128;
@@ -25,8 +25,8 @@ pub struct Meta {
     root2: u64,
 
     version: u64,
-    open: u8,
-    len: u64,
+    pub open: u8,
+    pub len: u64,
 }
 
 const_assert!(size_of::<Meta>() + CHECKSUM_SIZE < MIN_PAGE_SIZE as usize);
@@ -42,22 +42,20 @@ pub struct MetaHandler {
 
 struct Inner {
     version: u64,
-    index: usize,
+    is_first: bool,
     front: Box<[u8]>,
     back: Box<[u8]>,
 }
 
 impl MetaHandler {
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let file = File::open(path)?;
-
+    pub fn new(file: &File) -> Result<Self> {
         // TODO: if file does not exist, create it and write initial metadata
         // let page_size = choose_page_size(path.as_ref())?;
 
         let page_size = Self::read_page_size(&file)?;
         let buf1 = Self::read_buf(&file, page_size, 0)?;
         let buf2 = Self::read_buf(&file, page_size, page_size)?;
-        let (front, back) = Self::choose_front_back(buf1, buf2)?;
+        let (is_first, front, back) = Self::choose_front_back(buf1, buf2)?;
         let meta = from_bytes::<Meta>(&front);
 
         Ok(Self {
@@ -66,12 +64,56 @@ impl MetaHandler {
             root1: meta.root1,
             root2: meta.root2,
             inner: Mutex::new(Inner {
-                version: 0,
-                index: 0,
+                version: meta.version,
+                is_first,
                 front,
                 back,
             }),
         })
+    }
+
+    pub fn mutate(&self, file: &File, f: impl FnOnce(&mut Meta)) -> Result<()> {
+        let mut inner_guard = self.inner.blocking_lock();
+        let inner = &mut *inner_guard;
+
+        inner.version += 1;
+        inner.back.copy_from_slice(&inner.front);
+        let meta = from_bytes_mut::<Meta>(&mut inner.back);
+        f(meta);
+        meta.version = inner.version;
+        update_checksum(&mut inner.back);
+
+        let offset = if inner.is_first { 0 } else { self.page_size };
+        file.write_at(&inner.back, offset)?;
+
+        std::mem::swap(&mut inner.front, &mut inner.back);
+        inner.is_first = !inner.is_first;
+
+        Ok(())
+    }
+
+    pub async fn mutate_async(&self, fio: &Fio, f: impl FnOnce(&mut Meta)) -> Result<()> {
+        let mut inner_guard = self.inner.lock().await;
+        let inner = &mut *inner_guard;
+
+        inner.version += 1;
+        inner.back.copy_from_slice(&inner.front);
+        let meta = from_bytes_mut::<Meta>(&mut inner.back);
+        f(meta);
+        meta.version = inner.version;
+        update_checksum(&mut inner.back);
+
+        let pg_idx = if inner.is_first { 0 } else { 1 };
+        {
+            let mut buf = fio.get_buf();
+            buf.get_mut().copy_from_slice(&inner.back);
+            fio.write(pg_idx, buf).await?;
+        }
+
+        std::mem::swap(&mut inner.front, &mut inner.back);
+        inner.is_first = !inner.is_first;
+
+        Ok(())
     }
 
     fn read_page_size(file: &File) -> Result<u64> {
@@ -98,31 +140,27 @@ impl MetaHandler {
         Ok(buf.into())
     }
 
-    fn read_meta_from_buf(buf: &[u8]) -> &Meta {
-        unsafe { &*(buf.as_ptr() as *const Meta) }
-    }
-
-    fn choose_front_back(buf1: Box<[u8]>, buf2: Box<[u8]>) -> Result<(Box<[u8]>, Box<[u8]>)> {
+    fn choose_front_back(buf1: Box<[u8]>, buf2: Box<[u8]>) -> Result<(bool, Box<[u8]>, Box<[u8]>)> {
         let buf1_checksum_valid = has_valid_checksum(&buf1);
         let buf2_checksum_valid = has_valid_checksum(&buf2);
 
         if !buf1_checksum_valid && !buf2_checksum_valid {
             bail!("File corrupted, both metadata pages have invalid checksums");
         } else if buf1_checksum_valid && !buf2_checksum_valid {
-            Ok((buf1, buf2))
+            Ok((true, buf1, buf2))
         } else if !buf1_checksum_valid && buf2_checksum_valid {
-            Ok((buf2, buf1))
+            Ok((false, buf2, buf1))
         } else {
             // Both checksums are valid, choose the one with higher version
             let keep_order = {
-                let meta1 = Self::read_meta_from_buf(&buf1);
-                let meta2 = Self::read_meta_from_buf(&buf2);
+                let meta1 = from_bytes::<Meta>(&buf1);
+                let meta2 = from_bytes::<Meta>(&buf2);
                 meta1.version >= meta2.version
             };
             if keep_order {
-                Ok((buf1, buf2))
+                Ok((true, buf1, buf2))
             } else {
-                Ok((buf2, buf1))
+                Ok((false, buf2, buf1))
             }
         }
     }
