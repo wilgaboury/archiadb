5cd5debc0a2f1ca9e8c7902d1d41266cc7c1db99
Wil Gaboury <wgaboury@gmail.com>
2026-06-24 23:43:45 -0400

    Fix up txn map

--- src/txnmap.rs
+++ src/txnmap.rs
@@ -6,27 +6,37 @@ use crate::alloc::PageAllocator;
 
 /// Tracks currently running transactions and ensures that freeing of pages is deferred until no transactions reference them.
 pub(crate) struct TxnFreeDeferMap {
-    map: Mutex<BTreeMap<u64, Vec<u64>>>,
+    inner: Mutex<Inner>,
+}
+
+struct Inner {
+    next: u64,
+    map: BTreeMap<u64, Vec<u64>>,
 }
 
 impl TxnFreeDeferMap {
     pub fn new() -> Self {
         Self {
-            map: Mutex::new(BTreeMap::new()),
+            inner: Mutex::new(Inner {
+                next: 0,
+                map: BTreeMap::new(),
+            }),
         }
     }
 
     // TODO: I don't think it's okay to generate the id outside the lock, invalidates finishing assumptions
-    fn begin(&self, txn_id: u64) {
-        let mut map = self.map.lock();
-        map.insert(txn_id, Vec::with_capacity(0));
+    fn begin(&self) {
+        let mut inner = self.inner.lock();
+        let txn_id = inner.next;
+        inner.next += 1;
+        inner.map.insert(txn_id, Vec::with_capacity(0));
     }
 
     fn finish(&self, txn_id: u64, freeable: &mut Vec<u64>, alloc: &PageAllocator) {
         let free_pgs = {
-            let mut map = self.map.lock();
+            let mut inner = self.inner.lock();
             // Add pages to last transaction, since we can garuntee there will be no references to freed pages after it finishes
-            let last_txn = map.iter_mut().next_back();
+            let last_txn = inner.map.iter_mut().next_back();
             match last_txn {
                 Some((_, defer)) => defer.append(freeable),
                 None => {
@@ -36,9 +46,9 @@ impl TxnFreeDeferMap {
                 }
             }
 
-            let defer = map.remove(&txn_id);
+            let defer = inner.map.remove(&txn_id);
             if let Some(mut defer) = defer {
-                let next_back = map.range_mut(..txn_id).next_back();
+                let next_back = inner.map.range_mut(..txn_id).next_back();
                 match next_back {
                     Some((_, prev_defer)) => {
                         // Move pages to previous transaction, so they can be freed when it finishes
@@ -72,7 +82,7 @@ mod tests {
     use function_name::named;
 
     fn snapshot(map: &TxnFreeDeferMap) -> BTreeMap<u64, Vec<u64>> {
-        map.map.lock().clone()
+        map.inner.lock().map.clone()
     }
 
     #[named]
@@ -88,17 +98,17 @@ mod tests {
         let pg3 = alloc.alloc(&meta, &mut set).await?;
         set.flush().await?;
 
-        map.begin(1);
+        map.begin();
 
-        map.begin(2);
-        map.finish(2, &mut vec![pg2], &alloc);
+        map.begin();
+        map.finish(1, &mut vec![pg2], &alloc);
 
-        map.begin(3);
-        map.finish(3, &mut vec![pg3], &alloc);
+        map.begin();
+        map.finish(2, &mut vec![pg3], &alloc);
 
         let snap = snapshot(&map);
         println!("{:?}", snap);
-        assert_eq!(snap.get(&1), Some(&vec![pg2, pg3]));
+        assert_eq!(snap.get(&0), Some(&vec![pg2, pg3]));
         assert!(!snap.contains_key(&2));
         assert!(!snap.contains_key(&3));
 
@@ -106,7 +116,7 @@ mod tests {
         assert!(!alloc.is_free(pg2));
         assert!(!alloc.is_free(pg3));
 
-        map.finish(1, &mut vec![pg1], &alloc);
+        map.finish(0, &mut vec![pg1], &alloc);
 
         assert!(alloc.is_free(pg1));
         assert!(alloc.is_free(pg2));
@@ -128,25 +138,25 @@ mod tests {
         let pg3 = alloc.alloc(&meta, &mut set).await?;
         set.flush().await?;
 
-        map.begin(1);
-        map.begin(2);
-        map.begin(3);
+        map.begin();
+        map.begin();
+        map.begin();
 
-        map.finish(1, &mut vec![pg1], &alloc);
-
-        let snap = snapshot(&map);
-        assert_eq!(snap.get(&3), Some(&vec![pg1]));
-
-        map.finish(2, &mut vec![pg2], &alloc);
+        map.finish(0, &mut vec![pg1], &alloc);
 
         let snap = snapshot(&map);
-        assert_eq!(snap.get(&3), Some(&vec![pg1, pg2]));
+        assert_eq!(snap.get(&2), Some(&vec![pg1]));
+
+        map.finish(1, &mut vec![pg2], &alloc);
+
+        let snap = snapshot(&map);
+        assert_eq!(snap.get(&2), Some(&vec![pg1, pg2]));
 
         assert!(!alloc.is_free(pg1));
         assert!(!alloc.is_free(pg2));
         assert!(!alloc.is_free(pg3));
 
-        map.finish(3, &mut vec![pg3], &alloc);
+        map.finish(2, &mut vec![pg3], &alloc);
 
         assert!(alloc.is_free(pg1));
         assert!(alloc.is_free(pg2));
